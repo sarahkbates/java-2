92056e1cb0fe4ff3a6083946acc22cce344b6b40
Sarah Bates <skbates00@me.com>
2020-06-25 11:26:05 -0500

    finsihed exercise 1

--- burger_shop/Burger.java
+++ burger_shop/Burger.java
@@ -1,5 +1,57 @@
 package burger_shop;
 
+import java.text.DecimalFormat;
+
 public class Burger {
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    private String topping1Name;
+    private double topping1Price;
+
+    private String topping2Name;
+    private double topping2Price;
+
+    private static DecimalFormat priceFormat = new DecimalFormat("##0.00");
+
+
+
+    public Burger(String name, String meat, String breadRollType){
+        this.name = name;
+        this.meat = meat;
+        this.breadRollType = breadRollType;
+        this.price = 3.25;
+
+}
+
+    public void addBurgerTopping1(String name, double price){
+        this.topping1Name = name;
+        this.topping1Price = price;
+    }
+
+    public void addBurgerTopping2(String name, double price){
+        this.topping2Name = name;
+        this.topping2Price = price;
+    }
+
+    public double basicBurgerPrice(){
+        return this.price;
+    }
+
+    public double totalBurgerPrice(){
+        double burgerPrice = this.price;
+        System.out.println(this.name + " hamburger with a " + this.meat + " patty on a " + this.breadRollType + " roll, price is $" + priceFormat.format(this.price));
+        if(this.topping1Name != null){
+            burgerPrice += this.topping1Price;
+            System.out.println("Added " + this.topping1Name + " for an extra $" + priceFormat.format(this.topping1Price));
+        }
+        if (this.topping2Name != null){
+            burgerPrice += this.topping2Price;
+            System.out.println("Added " + this.topping2Name + " for an extra $" + priceFormat.format(this.topping2Price));
+        }
+        return burgerPrice;
+    }
 
 }
--- burger_shop/Main.java
+++ burger_shop/Main.java
@@ -1,7 +1,36 @@
 package burger_shop;
 
+import java.text.DecimalFormat;
+import java.util.HashMap;
+
 public class Main {
+
+    private static DecimalFormat priceFormat = new DecimalFormat("##0.00");
     public static void main(String[] args) {
         //main execution of the burger shop goes here.  This is the "entry point" of your application
+        Burger hamburger = new Burger("Basic", "Beef", "Wheat");
+        hamburger.addBurgerTopping1("Avocado", 1.50);
+        hamburger.addBurgerTopping2("Ketchup", 0.25);
+        System.out.println("Basic Burger price: $" +priceFormat.format(hamburger.basicBurgerPrice()));
+
+        DeluxeBurger deluxeBurger = new DeluxeBurger("White");
+        deluxeBurger.addBurgerTopping1("Avocado", 1.50);
+        deluxeBurger.addBurgerTopping2("topping 2", 0.25);
+        deluxeBurger.addBurgerTopping3("topping 3", 0.50);
+        deluxeBurger.addBurgerTopping4("topping 4", 0.75);
+        deluxeBurger.addBurgerTopping5("topping 5", 1.00);
+        System.out.println("Deluxe Burger price: $" +priceFormat.format(deluxeBurger.basicBurgerPrice()));
+        System.out.println("Total price after toppings: $" + priceFormat.format(deluxeBurger.totalBurgerPrice()));
+
+        HealthBurger health = new HealthBurger("Turkey");
+        health.addHealthTopping1("Lettuce", 0.50);
+        health.addHealthTopping2("Bacon", 1.25);
+        health.addHealthTopping3("Pickle", 0.50);
+        health.addHealthTopping4("Mustard", 0.25);
+        System.out.println("Heath Burger price: $" + priceFormat.format(health.basicBurgerPrice()));
+        System.out.println("Total after toppings: $" + priceFormat.format(health.totalBurgerPrice()));
+
+
+        
     }
 }
